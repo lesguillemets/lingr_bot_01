718a6d4ce130c714ebbdc3842c6e79988c907b58
exumbra_insolem <lesguillemets@github.fake>
2014-01-28 23:23:13 +0900

    dictionarize.

--- bot.py
+++ bot.py
@@ -8,12 +8,91 @@ import re
 URLPATTERN = re.compile(r'https?://\S+')
 
 class LingrBot1(webapp2.RequestHandler):
+    def __init__(self, *args):
+        super(LingrBot1, self).__init__(*args)
+        self.commands = {
+            '!ping' : self.do_ping,
+            '!weather' : self.do_weather,
+            '!bf' : self.do_bf,
+            '!pyref' : self.do_pyref,
+            '!pyref2' : self.do_pyref2,
+            '!pyref3' : self.do_pyref3,
+            '!random' : self.do_random,
+            '!google' : self.do_google,
+            '!gimg' : self.do_gimg,
+            '!nicodic' : self.do_nicodic,
+            '!timezzz' : self.do_timezzz,
+            u'今何時ぢゃ' : self.do_koku,
+        }
+    
     def get(self):
         self.response.write("""Hi""")
     
     def post(self):
         #self.do(self.request.body)
         self.do(json.loads(self.request.body))
+    
+    def do_ping(self, body):
+        self.response.write(self.ping())
+    
+    def do_weather(self, body):
+        from modules import weather 
+        forecast = weather.WForecast(text.split()[1]).forecast()
+        self.response.write(forecast)
+    
+    def do_bf(self, body):
+        self.response.write("BF intepret result:\n")
+        from modules import bf
+        mybf = bf.BrainSth(30)
+        mybf.give_code(body)
+        self.response.write(mybf.execute())
+    
+    def do_pyref(self, body):
+        from modules import pyref
+        self.response.write(pyref.pyref(body))
+    
+    def do_pyref3(self, body):
+        from modules import pyref
+        self.response.write(pyref.pyref3(body))
+    
+    def do_pyref2(self, body):
+        from modules import pyref
+        self.response.write(pyref.pyref(body, '2'))
+    
+    def do_random(self, body):
+        import random
+        if body:
+            try:
+                self.response.write(random.randint(0, int(body)))
+            except ValueError:
+                pass
+        else:
+            self.response.write(random.random())
+    
+    def do_nicodic(self, body):
+        from modules import nicodic
+        self.response.write(nicodic.nicodic(body.encode('utf-8')))
+    
+    def do_timezzz(self, body):
+        import datetime as dt
+        self.response.write(dt.datetime.now().__repr__())
+    
+    def do_google(self, body):
+        from modules import googlesearch as gs
+        self.response.write(gs.gslink(body))
+    
+    def do_gimg(self, body):
+        from modules import googlesearch as gs
+        self.response.write(gs.gslink_img(body))
+    
+    def do_koku(self, body):
+        from modules import etotime
+        import datetime as dt
+        etime = etotime.ETOTime()
+        self.response.write(
+            etime.koku(dt.datetime.now() + dt.timedelta(hours=9)) 
+            + u" にて候")
+    
     
     def do(self, data):
         message = data['events'][0]['message']
@@ -23,73 +102,15 @@ class LingrBot1(webapp2.RequestHandler):
             body = text[text.index(' ')+1:]
         else:
             body = ''
+        
         # command execution
-        
-        if command == '!ping': # ping
-            self.response.write(self.ping())
-        
-        elif command == '!weather': # weather forecasts
-            from modules import weather 
-            forecast = weather.WForecast(text.split()[1]).forecast()
-            self.response.write(forecast)
-        
-        elif command == '!bf': # bf interpreter
-            self.response.write("BF intepret result:\n")
-            from modules import bf
-            mybf = bf.BrainSth(30)
-            mybf.give_code(body)
-            self.response.write(mybf.execute())
+        if command in self.commands:
+            self.commands[command](body)
         
         elif command[0] == '!' and command[-2:] == 'wp': # wp article
             from modules import wparticle
             langcode = command[1:-2]
             self.response.write(wparticle.wparticle(langcode, body))
-        
-        elif command == '!pyref':
-            from modules import pyref
-            self.response.write(pyref.pyref(body))
-        
-        elif command == '!pyref3':
-            from modules import pyref
-            self.response.write(pyref.pyref3(body))
-        
-        elif command == '!pyref2':
-            from modules import pyref
-            self.response.write(pyref.pyref(body, '2'))
-        
-        elif command == '!random':
-            import random
-            if body:
-                try:
-                    self.response.write(random.randint(0, int(body)))
-                except ValueError:
-                    pass
-            else:
-                self.response.write(random.random())
-        
-        elif command == '!nicodic':
-            from modules import nicodic
-            self.response.write(nicodic.nicodic(body.encode('utf-8')))
-        
-        elif command == '!timezzz':
-            import datetime as dt
-            self.response.write(dt.datetime.now().__repr__())
-        
-        elif command == '!google':
-            from modules import googlesearch as gs
-            self.response.write(gs.gslink(body))
-        
-        elif command == '!gimg':
-            from modules import googlesearch as gs
-            self.response.write(gs.gslink_img(body))
-        
-        elif command == u"今何時ぢゃ":
-            from modules import etotime
-            import datetime as dt
-            etime = etotime.ETOTime()
-            self.response.write(
-                etime.koku(dt.datetime.now() + dt.timedelta(hours=9)) 
-                + u" にて候")
         
         else: # not command
             if has_url(text):
